5835eb38a98963edbf75475a02b2049fe20ac0b9
Michael Rashkovetskyi <misharash@gmail.com>
2024-02-12 18:57:14 -0500

    Y1 post-recon single EZmock scripts

---
+++ DESI-Y1-EZmocks-LRG-post-single/run_covs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --qos=regular
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=RascalC-Y1-EZmocks-recon-single
+#SBATCH --array=0-19 # 10 mocks, SGC and NGC
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+# GSL needed by the C++ code should already be loaded in cosmodesi
+# module load gsl
+
+# OpenMP settings
+# export OMP_PROC_BIND=spread
+# export OMP_PLACES=threads
+# export OMP_NUM_THREADS=256
+
+# Hopefully let numpy use all threads
+# export NUMEXPR_MAX_THREADS=256
+# Limit OpenBLAS thread usage (for jackknife assignment, error otherwise)
+# export OPENBLAS_NUM_THREADS=1
+
+python -u run_covs.py $SLURM_ARRAY_TASK_ID
