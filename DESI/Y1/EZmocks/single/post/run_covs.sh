1bdf7703dd96a449da064e9094d4c5d8c99cbbc2
Michael Rashkovetskyi <misharash@gmail.com>
2024-02-19 17:04:42 -0800

    Re-submit underconverged jobs

--- DESI/Y1/EZmocks/single/post/run_covs.sh
+++ DESI/Y1/EZmocks/single/post/run_covs.sh
@@ -6,7 +6,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-EZmocks-recon-single
-#SBATCH --array=20-59 # 10 mocks, SGC and NGC, no LRG but ELG and BGS
+#SBATCH --array=20,25,34,44,49,50,52,58,59 # 10 mocks, SGC and NGC, no LRG but ELG and BGS
+##SBATCH --array=0-59 # full job array: 10 mocks, SGC and NGC, LRG, ELG and BGS
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
