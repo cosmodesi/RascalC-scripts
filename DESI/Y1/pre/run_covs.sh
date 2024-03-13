d1a2fbc3d3ada979e212490d296851e74f97bddf
Michael Rashkovetskyi <misharash@gmail.com>
2024-03-13 09:58:54 -0700

    Relaunched underconverged pre-recon data jobs

--- DESI/Y1/pre/run_covs.sh
+++ DESI/Y1/pre/run_covs.sh
@@ -6,7 +6,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=RascalC-Y1-v1.2-unblinded
-#SBATCH --array=0-5,8-11,14-17 # full job array: excluding LRG and ELG full ranges
+#SBATCH --array=2,11 # underconverged jobs
+##SBATCH --array=0-5,8-11,14-17 # full job array: excluding LRG and ELG full ranges
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
