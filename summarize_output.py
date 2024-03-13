8a8a56e6c051a0bb735e53ca39673d9a95ccec1d
Michael Rashkovetskyi <misharash@gmail.com>
2024-03-13 09:47:26 -0700

    Added rescaling reporting in the summary script

--- summarize_output.py
+++ summarize_output.py
@@ -9,6 +9,7 @@ for filename in sys.argv[1:]:
     print(f"\nSummary of {filename}:")
     start_time, time = None, None
     R_inv = []
+    rescaling = None
     for line in open(filename):
         # print if warning or error
         if any(word in line.lower() for word in print_if_words):
@@ -17,6 +18,9 @@ for filename in sys.argv[1:]:
         if line.startswith("RMS eigenvalues"):
             line_parts = line.split()
             R_inv += [float(line_parts[i]) for i in (-3, -1)]
+        # get shot-noise rescaling
+        elif line.startswith("Optimization complete - optimal rescaling parameter"):
+            rescaling = float(line.split()[-1])
         # try to get timestamps
         try:
             time = datetime.fromisoformat(line.strip())
@@ -31,5 +35,8 @@ for filename in sys.argv[1:]:
         if len(R_inv) >= 8:
             R_inv_jack = np.around(100 * np.sort(R_inv[4:8])[::-1], 1)
             print("R_inv jack:", *R_inv_jack, "%")
+    # report shot-noise rescaling if retrieved
+    if rescaling:
+        print(f"Optimal shot-noise rescaling: {rescaling:.2f}")
 
 print()
