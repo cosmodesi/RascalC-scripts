736762fdcc7bcb74d8983c0884095f3baf1d1ece
Michael Rashkovetskyi <misharash@gmail.com>
2024-07-03 11:16:07 +0300

    New QSO reconstruction scheme setup

---
+++ DESI/Y1/post/recon_growth_bias_weights/run_covs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --qos=shared
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
+#SBATCH --job-name=RascalC-Y1-v1.2-unblinded-recon-growth-bias-weights
+#SBATCH --array=16,17 # QSO only
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+# GSL needed by the C++ code should already be loaded in cosmodesi
+# module load gsl
+
+# OpenMP settings
+# export OMP_PROC_BIND=spread
+# export OMP_PLACES=threads
+# export OMP_NUM_THREADS=256
+
+# Hopefully let numpy use all threads
+# export NUMEXPR_MAX_THREADS=256
+# Limit OpenBLAS thread usage (for jackknife assignment, error otherwise)
+# export OPENBLAS_NUM_THREADS=1
+
+python -u run_covs.py $SLURM_ARRAY_TASK_ID
