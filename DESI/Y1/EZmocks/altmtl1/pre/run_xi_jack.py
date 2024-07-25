6401f8b7dad48afa91524c7c5ad3be9a8beb0385
Michael Rashkovetskyi <misharash@gmail.com>
2024-07-25 13:28:02 -0700

    Fixes/improvements for jackknife counts scripts

--- DESI/Y1/EZmocks/altmtl1/pre/run_xi_jack.py
+++ DESI/Y1/EZmocks/altmtl1/pre/run_xi_jack.py
@@ -2,7 +2,7 @@
 
 import os
 import numpy as np
-from tqdm import trange
+import logging
 from astropy.table import Table, vstack
 import desi_y1_files.file_manager as desi_y1_file_manager
 from RascalC.pre_process import get_subsampler_xirunpc
@@ -20,18 +20,23 @@ def prepare_catalog(filename: str, z_min: float = -np.inf, z_max: float = np.inf
     catalog.keep_columns(["RA", "DEC", "Z", "WEIGHT"]) # discard everything else
     filtering = np.logical_and(catalog["Z"] >= z_min, catalog["Z"] <= z_max) # logical index of redshifts within the range
     catalog = catalog[filtering] # filtered catalog
+    for key in catalog.keys():
+        if catalog[key].dtype != float:
+            catalog[key] = catalog[key].astype(float) # ensure everything is float(64)
     catalog["comov_dist"] = cosmology.comoving_radial_distance(catalog["Z"])
     if jack_sampler: catalog["JACK"] = jack_sampler.label(get_rdd_positions(catalog), position_type = 'rdd')
     return catalog
 
 
 setup_logging()
+my_logger = logging.getLogger()
 cosmology = TabulatedDESI()
 
 mock_id = 1
 
-input_dir = f"/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/ALTMTL_EZmock/altmtl{mock_id}/mock{mock_id}/LSScats"
-output_dir = f"{input_dir}/xi/smu/"
+input_dir = f"/dvs_ro/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/ALTMTL_EZmock/altmtl{mock_id}/mock{mock_id}/LSScats" # read-only mount for speed
+output_dir = "xi/smu" # here in scratch for faster write, to be moved later
+os.makedirs(output_dir, exist_ok = True)
 
 n_mu_bins = 200 # between -1 and 1
 s_max = 200
@@ -41,16 +46,19 @@ all_edges = ((s_edges, np.linspace(-1, 1, n_mu_bins+1)) for s_edges in (np.arang
 
 n_jack = 60
 
-for tracer, z_ranges in desi_y1_file_manager.list_zrange:
+for tracer, z_ranges in desi_y1_file_manager.list_zrange.items():
     if tracer.startswith("BGS"): continue # skip BGS
     if "+" in tracer: continue # skip the combined tracer (LRG+ELG_LOPnotqso)
     n_randoms = desi_y1_file_manager.list_nran[tracer]
+    my_logger.info(f"Tracer: {tracer}")
 
     for (z_min, z_max) in z_ranges:
+        my_logger.info(f"Redshift range: {z_min}-{z_max}")
         for reg in ("SGC", "NGC"):
+            my_logger.info(f"Region: {reg}")
             output_file = f"{output_dir}/allcounts_{tracer}_{reg}_z{z_min}-{z_max}_default_FKP_lin_nran{n_randoms}_njack{n_jack}_split{split_above}.npy"
             if os.path.exists(output_file):
-                print(f"Output file {output_file} exists, skipping")
+                my_logger.info(f"Output file {output_file} exists, skipping")
                 continue
 
             galaxies = prepare_catalog(f"{input_dir}/{tracer}_{reg}_clustering.dat.fits", z_min, z_max)
@@ -62,9 +70,11 @@ for tracer, z_ranges in desi_y1_file_manager.list_zrange:
             results = []
             # compute
             for i_split_randoms, edges in enumerate(all_edges):
+                my_logger.info("Using " + ("split" if i_split_randoms else "concatenated") + " randoms")
                 result = 0
                 D1D2 = None
-                for i_random in trange(n_randoms if i_split_randoms else 1, desc = "Computing xi with random"):
+                for i_random in range(n_randoms if i_split_randoms else 1):
+                    if i_split_randoms: my_logger.info(f"Split random {i_random+1} of {n_randoms}")
                     these_randoms = all_randoms[i_random] if i_split_randoms else vstack(all_randoms)
                     tmp = TwoPointCorrelationFunction(mode = 'smu', edges = edges,
                                                     data_positions1 = get_rdd_positions(galaxies), data_weights1 = galaxies["WEIGHT"], data_samples1 = galaxies["JACK"],
--- DESI/Y1/EZmocks/altmtl1/pre/run_xi_jack.sh
+++ DESI/Y1/EZmocks/altmtl1/pre/run_xi_jack.sh
@@ -1,2 +1,3 @@
 # interactive launch, NOT a batch script
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 srun --account=desi_g --constraint=gpu --qos=interactive --time=4:00:00 --nodes=1 --gpus=4 --job-name=allcounts-EZmock-altmtl1-jack python -u run_xi_jack.py
