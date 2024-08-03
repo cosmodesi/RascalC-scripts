d0d04ccbbd073b8db7ef9e9f24d434a7fd0619f7
Michael Rashkovetskyi <misharash@gmail.com>
2024-08-02 19:35:42 -0700

    Covariance scripts for Abacus FFA

---
+++ DESI/Y1/Abacus/pre/ffa/run_covs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --qos=shared
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
+#SBATCH --job-name=RascalC-Y1-Abacus-v4_2-ffa
+#SBATCH --array=4,5,10,11 # only higher-z ranges of LRG and ELG
+##SBATCH --array=0-5,8-11,14-17 # full job array: excluding LRG and ELG full ranges
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+python -u run_covs.py $SLURM_ARRAY_TASK_ID
