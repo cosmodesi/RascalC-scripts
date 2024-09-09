c3785282d09a36d5a4372679d24302f1162e7665
Michael Rashkovetskyi <misharash@gmail.com>
2024-09-09 11:14:12 -0700

    Cov run scripts for DESI Y3 pre-recon data
    Mostly copied from Y1, but removed the full ranges for LRG and ELG and
    cleared the loop number tweaks

---
+++ DESI/Y3/pre/run_covs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --qos=shared
+#SBATCH --time=18:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
+#SBATCH --job-name=RascalC-Y3-jura-v0.1-BAO-blinded
+#SBATCH --array=0-13 # full job array
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+python -u run_covs.py $SLURM_ARRAY_TASK_ID
