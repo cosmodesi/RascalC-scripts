f52cf126ac2d3fabd30f586573a40f350c3946cb
Michael Rashkovetskyi <misharash@gmail.com>
2024-10-09 18:06:05 -0700

    Re-run underconverged computations

--- DESI/Y3/post/run_covs.sh
+++ DESI/Y3/post/run_covs.sh
@@ -6,7 +6,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
 #SBATCH --job-name=RascalC-Y3-v1-BAO-blinded-recon
-#SBATCH --array=0-15 # full job array
+##SBATCH --array=0-15 # full job array
+#SBATCH --array=3,12,13 # underconverged LRG 0.6-0.8 NGC and BGS_BRIGHT-20.2
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
