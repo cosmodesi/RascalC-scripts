9a90ff16f78f4b6addcb50b9dd87b88928938624
Michael Rashkovetskyi <misharash@gmail.com>
2024-11-21 08:59:01 -0800

    Bash scripts for file access testing

---
+++ DESI/Y3/post/run_covs_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+for i in {{0..15},22,23}; do
+    echo ID $i
+    python -u run_covs.py -t $i
+done
