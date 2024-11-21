38a61561d9df75e2b5a0bc33a8dd4bf7fddfb2e6
Michael Rashkovetskyi <misharash@gmail.com>
2024-11-21 12:45:16 -0800

    Set up Y3 LRG&ELG pre-recon covariances

---
+++ DESI/Y3/multi/pre/run_covs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --qos=shared
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
+#SBATCH --job-name=RascalC-Y3-v1.1-BAO-blinded-LRGxELG
+#SBATCH --array=0-1
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+python -u run_covs.py $SLURM_ARRAY_TASK_ID
