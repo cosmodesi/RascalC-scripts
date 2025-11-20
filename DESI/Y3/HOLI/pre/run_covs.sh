146b264105cb401784e7765aebb41690979ba27e
Michael Rashkovetskyi <misharash@gmail.com>
2025-11-20 13:44:06 -0800

    Copied covariance scripts for HOLI mocks

---
+++ DESI/Y3/HOLI/pre/run_covs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --qos=shared
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
+#SBATCH --job-name=RascalC-Y3-HOLI
+##SBATCH --array=0-23 # full job array
+##SBATCH --array=0-11,14,15,22,23 # only -21.35 z0.1-0.4 for BGS, and all other tracers
+#SBATCH --array=0-5 # only LRG
+
+# load cosmodesi environment
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh test
+
+python -u run_covs.py $SLURM_ARRAY_TASK_ID
