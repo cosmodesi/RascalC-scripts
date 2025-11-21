3f242b5b77181c0f62675d501d0c0e5baee59711
Michael Rashkovetskyi <misharash@gmail.com>
2025-11-21 08:54:51 -0800

    Re-run one of the HOLI covs a bit longer

--- DESI/Y3/HOLI/pre/run_covs.sh
+++ DESI/Y3/HOLI/pre/run_covs.sh
@@ -8,7 +8,8 @@
 #SBATCH --job-name=RascalC-Y3-HOLI
 ##SBATCH --array=0-23 # full job array
 ##SBATCH --array=0-11,14,15,22,23 # only -21.35 z0.1-0.4 for BGS, and all other tracers
-#SBATCH --array=0-5 # only LRG
+##SBATCH --array=0-5 # only LRG
+#SBATCH --array=0 # re-run the badly converged case
 
 # load cosmodesi test environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh test
