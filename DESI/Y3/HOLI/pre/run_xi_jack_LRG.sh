e6430805eb5a5764159aaeaa9bc7169bf6d828c0
Michael Rashkovetskyi <misharash@gmail.com>
2025-12-18 11:48:37 -0800

    Updated the 2PCF computation script for HOLI

--- DESI/Y3/HOLI/pre/run_xi_jack_LRG.sh
+++ DESI/Y3/HOLI/pre/run_xi_jack_LRG.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# load cosmodesi environment, test version
-source /global/common/software/desi/users/adematti/cosmodesi_environment.sh test
+# load cosmodesi environment, 2025_05 (old test) version
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh 2025_05
 
 LSSCODE=$HOME
+BASEDIR=/dvs_ro/cfs/cdirs/desi/survey/catalogs/DA2/mocks/Holi/seed0202/altmtl202/loa-v1/mock202/LSScats/
 
-srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g python $LSSCODE/LSS/scripts/xirunpc.py --gpu --tracer LRG --survey DA2 --verspec loa-v1 --version test --region NGC SGC --corr_type smu --njack 60 --weight_type default_FKP --basedir /dvs_ro/cfs/cdirs/desi/survey/catalogs/DA2/mocks/Holi/seed0202/altmtl202/loa-v1/mock202/LSScats/ --outdir xi
+srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g python $LSSCODE/LSS/scripts/xirunpc.py --gpu --tracer LRG --survey DA2 --verspec loa-v1 --version test --region NGC SGC --corr_type smu --njack 60 --weight_type default_FKP --basedir $BASEDIR --outdir xi
