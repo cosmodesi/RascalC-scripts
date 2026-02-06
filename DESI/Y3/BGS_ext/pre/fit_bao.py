ff5254d05068de15eb539c32c3ac74f36001e5bc
Michael Rashkovetskyi <misharash@gmail.com>
2026-02-06 11:45:50 -0800

    BAO fit script for BGS_ANY-21.35

---
+++ DESI/Y3/BGS_ext/pre/fit_bao.py
@@ -0,0 +1,100 @@
+import numpy as np
+import jax
+jax.config.update('jax_platform_name', 'cpu')
+from desilike.theories.galaxy_clustering import BAOPowerSpectrumTemplate, DampedBAOWigglesTracerCorrelationFunctionMultipoles
+from desilike.observables.galaxy_clustering import TracerCorrelationFunctionMultipolesObservable
+from desilike.likelihoods import ObservablesGaussianLikelihood
+from desilike.profilers import MinuitProfiler
+from desilike import setup_logging
+from pycorr import TwoPointCorrelationFunction
+import os
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+
+# Settings for filenames
+verspec = 'loa-v1'
+version = "v2"
+conf = "PIP"
+
+REDSHIFTS = {'BGS_ANY-21.35': [(0.1, 0.4)]}
+EFFECTIVE_REDSHIFTS = {'BGS_ANY-21.35': [0.2]} # need to do better
+SMOOTHINGS = {'BGS_ANY-21.35': 15} # only post-recon
+
+CAPS = ["GCcomb", "NGC", "SGC"]
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verspec", type = str, default = 'loa-v1')
+    parser.add_argument("--version", type = str, default = 'v2')
+    parser.add_argument("--conf", type = str, default = 'PIP')
+    parser.add_argument("--seed", type = int, default = 42)
+    parser.add_argument("--rerun", action = 'store_true')
+    parser.add_argument("--smin", type = float, default = 48)
+    parser.add_argument("--smax", type = float, default = 152)
+    parser.add_argument("--ds", type = float, default = 4)
+    parser.add_argument("--max_l", type = int, default = 2)
+    
+
+    args = parser.parse_args()
+    
+    ells = np.arange(0, args.max_l + 1, 2)
+    smin, smax, ds = args.smin, args.smax, args.ds
+    cov_dir = f"cov_txt/{args.verspec}/{args.version}/{args.conf}"
+    xi_dir = f"{args.verspec}/LSScats/{args.version}/{args.conf}/xi/smu"
+    output_basedir = f"{args.verspec}/LSScats/{args.version}/{args.conf}/fits"
+    for tracer in REDSHIFTS.keys():
+        for zeff, (zmin, zmax) in zip(EFFECTIVE_REDSHIFTS[tracer], REDSHIFTS[tracer]):
+            for cap in CAPS:
+                print(f"Starting fit for {tracer} z={zmin}-{zmax} {cap}", flush = True)
+
+                xi_fn = f"{xi_dir}/allcounts_{tracer}_{cap}_{zmin}_{zmax}_default_FKP_lin_njack60_nran1_split20.npy"
+                cov_fn = f"{cov_dir}/xi024_{tracer}_{cap}_z{zmin}-{zmax}_default_FKP_lin{ds}_s20-200_cov_RascalC.txt"
+                
+                print(f"Using covariance saved in {cov_fn}", flush = True)
+                if not os.path.isfile(cov_fn):
+                    print(f"{cov_fn} not found!")
+                    continue
+                cov_rc = np.loadtxt(cov_fn)
+                # cut the covariance
+                cov_rc_l = np.concatenate([np.tile(ell, 45) for ell in (0, 2, 4)])
+                cov_rc_s = np.concatenate([np.arange(20, 200, 4) + 2 for ell in (0, 2, 4)])
+                cov_rc_cut_1d = np.logical_and(cov_rc_l <= args.max_l, np.logical_and(cov_rc_s >= args.smin, cov_rc_s <= args.smax))
+                cov_rc_cut_2d = np.ix_(cov_rc_cut_1d, cov_rc_cut_1d)
+                covariance = cov_rc[cov_rc_cut_2d]
+                
+                
+                
+                output_dir = f"desilike_bao_minuit_xi_{tracer}_{cap}_z{zmin:.1f}-{zmax:.1f}/"
+                os.makedirs(output_dir, exist_ok = True)
+                    
+                setup_logging()
+                    
+                template = BAOPowerSpectrumTemplate(z = zeff, fiducial = 'DESI', apmode = 'qisoqap')
+                theory = DampedBAOWigglesTracerCorrelationFunctionMultipoles(template=template, broadband='pcs2') # pre-recon, for post-recon need mode="recsym" or "reciso"
+                
+                output_fn = f"{output_dir}/minuit_prof.txt"
+                if os.path.isfile(output_fn + ".npy") and not args.rerun: continue
+                observable = TracerCorrelationFunctionMultipolesObservable(data = TwoPointCorrelationFunction.load(xi_fn)[smin:smax:ds],  
+                                                                    covariance = covariance,
+                                                                    slim = {ell: [smin, smax, ds] for ell in ells},
+                                                                    theory = theory,
+                                                                    wmatrix = {'resolution': 1})
+                likelihood = ObservablesGaussianLikelihood(observables=[observable])
+                for param in likelihood.all_params.select(basename=['alpha*', 'sn*']):
+                    param.update(derived='.auto')
+                    
+                
+                if os.path.isfile(output_fn+".npy"): os.remove(output_fn+".npy")
+                profiler = MinuitProfiler(likelihood, save_fn = output_fn+".npy", seed = args.seed, mpicomm = comm)
+                profiles = profiler.maximize(niterations=3)
+                # To print relevant information
+                print(profiles.to_stats(tablefmt='pretty', fn = output_fn))
+                profiles.save(output_fn+".npy")
+                #if j == 1:
+                # likelihood(**profiler.profiles.bestfit.choice(input=True))
+                # observable.plot()
+                # plt.gcf()
+                # plt.savefig("plots/SecondGen/desilike_ez_v_rc_direct_bestfit.png")
+            
