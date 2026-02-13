050cc14335c2e4f70bbfebd6769acc3e489434f7
Michael Rashkovetskyi <misharash@gmail.com>
2026-02-13 13:56:31 -0800

    Fixed BGS_BRIGHT-21.35 test IDs

--- DESI/Y3/BGS_ext/pre/run_covs_test.sh
+++ DESI/Y3/BGS_ext/pre/run_covs_test.sh
@@ -3,8 +3,8 @@
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 
-# for i in {0..4}; do
-for i in {3,4}; do
+# for i in {0..3}; do
+for i in {2,3}; do
     echo ID $i
     python -u run_covs.py -t $i
 done
