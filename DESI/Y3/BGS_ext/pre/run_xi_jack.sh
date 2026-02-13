e1612f400ac00a9bab42c1c8ff2e1fbfe16bbcc5
Michael Rashkovetskyi <misharash@gmail.com>
2026-02-13 11:17:57 -0800

    Updated xi for BGS_BRIGHT-21.35

--- DESI/Y3/BGS_ext/pre/run_xi_jack.sh
+++ DESI/Y3/BGS_ext/pre/run_xi_jack.sh
@@ -7,4 +7,11 @@ LSSCODE=$HOME
 BASEDIR=/dvs_ro/cfs/cdirs/desi/survey/catalogs/DA2/LSS/loa-v1/LSScats/v2/PIP/
 BASE_OUTDIR=loa-v1/LSScats/v2/PIP/
 
-srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g python $LSSCODE/LSS/scripts/xirunpc.py --gpu --tracer BGS_ANY-21.35 --nran 1 --region NGC SGC --corr_type smu --njack 60 --weight_type default_FKP --basedir $BASEDIR --outdir $BASE_OUTDIR/xi
+# srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g python $LSSCODE/LSS/scripts/xirunpc.py --gpu --tracer BGS_ANY-21.35 --nran 1 --region NGC SGC --corr_type smu --njack 60 --weight_type default_FKP --basedir $BASEDIR --outdir $BASE_OUTDIR/xi
+
+srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g python $LSSCODE/LSS/scripts/xirunpc.py --gpu --tracer BGS_BRIGHT-21.35 --nran 1 --region NGC SGC --corr_type smu --njack 60 --weight_type default_FKP --basedir $BASEDIR --outdir $BASE_OUTDIR/xi
+
+BASEDIR=/dvs_ro/cfs/cdirs/desi/survey/catalogs/DA2/LSS/loa-v1/LSScats/v2/nonKP/
+BASE_OUTDIR=loa-v1/LSScats/v2/nonKP/
+
+srun -N 1 -C gpu -t 04:00:00 --gpus 4 --qos interactive --account desi_g python $LSSCODE/LSS/scripts/xirunpc.py --gpu --tracer BGS_BRIGHT-21.35 --nran 1 --region NGC SGC --corr_type smu --njack 60 --weight_type default_FKP --basedir $BASEDIR --outdir $BASE_OUTDIR/xi
