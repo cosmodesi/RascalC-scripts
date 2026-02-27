855866a5db163cd0ba0ac9c52fad5af887a3e869
Michael Rashkovetskyi <misharash@gmail.com>
2026-02-27 11:58:29 -0800

    Attempt to fix pre-recon tracer separation
    Added BGS, still doesn't work

--- DESI/Y3/comb/pre_as_multi/label_tracers_full.py
+++ DESI/Y3/comb/pre_as_multi/label_tracers_full.py
@@ -35,7 +35,7 @@ os.environ["DESICFS"] = "/dvs_ro/cfs/cdirs/desi" # read-only mount works faster,
 
 fm = desi_y3_file_manager.get_data_file_manager(conf, verspec)
 
-separate_tracers = ['LRG', 'ELG_LOPnotqso', 'QSO'] # tracers to split the combined tracer into
+separate_tracers = ['BGS_BRIGHT-21.35', 'LRG', 'ELG_LOPnotqso', 'QSO'] # tracers to split the combined tracer into
 combined_tracer = 'FullCombined' # the combined tracer
 corr_labels = [separate_tracers[0], "_".join(separate_tracers), separate_tracers[1]]
 
