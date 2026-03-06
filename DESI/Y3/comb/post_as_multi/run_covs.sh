0e8698db5bd3ae5191927bcb7eb5514cb252baf0
Michael Rashkovetskyi <misharash@gmail.com>
2026-03-06 12:18:16 -0800

    Run covariance for full combined as multi (post)

--- DESI/Y3/comb/post_as_multi/run_covs.sh
+++ DESI/Y3/comb/post_as_multi/run_covs.sh
@@ -5,8 +5,9 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
-#SBATCH --job-name=RascalC-Y3-v1.1-LRG+ELG-as-multi-post
-#SBATCH --array=0-1
+#SBATCH --job-name=RascalC-Y3-v1.1-combined-tracer-as-multi-post
+##SBATCH --array=0-3 # full job array
+#SBATCH --array=2-3
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
