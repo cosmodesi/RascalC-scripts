11669de799968e197429cd8630fe9fe61f1b644d
Michael Rashkovetskyi <misharash@gmail.com>
2026-04-27 14:15:31 -0700

    Tiny fix in job name

--- DESI/Y3/pre/updated/run_covs.sh
+++ DESI/Y3/pre/updated/run_covs.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
-#SBATCH --job-name=RascalC-Y3-HOLI
+#SBATCH --job-name=RascalC-Y3-v2
 ##SBATCH --array=0-23 # full job array
 ##SBATCH --array=0-11,14,15,22,23 # only -21.35 z0.1-0.4 for BGS, and all other tracers
 #SBATCH --array=0-9,22,23 # LRG, ELG and QSO
