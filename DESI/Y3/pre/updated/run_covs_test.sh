86690a62567cef0584cf9ab2bb9d0a2fe956e6c3
Michael Rashkovetskyi <misharash@gmail.com>
2026-05-01 13:59:56 -0700

    Restored full test for data

--- DESI/Y3/pre/updated/run_covs_test.sh
+++ DESI/Y3/pre/updated/run_covs_test.sh
@@ -3,8 +3,7 @@
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 
-# for i in {{0..9},14,15,22,23}; do
-for i in {14,15}; do
+for i in {{0..9},14,15,22,23}; do
     echo ID $i
     python -u run_covs.py -t $i
 done
