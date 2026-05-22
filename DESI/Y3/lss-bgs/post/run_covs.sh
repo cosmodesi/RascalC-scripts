67dbdc3bfbcb2f2f8e5f71d4e8a7937ec0d78533
Michael Rashkovetskyi <misharash@gmail.com>
2026-05-22 14:13:28 -0700

    Updated and relaunched a couple of BGS cases

--- DESI/Y3/lss-bgs/post/run_covs.sh
+++ DESI/Y3/lss-bgs/post/run_covs.sh
@@ -7,7 +7,8 @@
 #SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
 #SBATCH --job-name=RascalC-LSS-BGS-recon
 # Choose/edit job array to match the campaign; double-check with `python run_covs.py --list-cases --campaign <name>`
-#SBATCH --array=0-17 # nonkp_bright_faint_priority
+##SBATCH --array=0-17 # nonkp_bright_faint_priority
+#SBATCH --array=2 # nonkp_bright_faint_priority re-run
 ##SBATCH --array=0-13 # nonkp_bright_compare or legacy
 ##SBATCH --array=0-29 # pip_priority
 ##SBATCH --array=0-19 # pip_20p7
