bf3a0f7cd3d9c3f62789151947278c18c77c9d41
Michael Rashkovetskyi <misharash@gmail.com>
2026-05-25 22:57:22 -0400

    Propagated the change into run_covs.sh copy

--- DESI/Y3/lss-bgs/post/run_covs2.sh
+++ DESI/Y3/lss-bgs/post/run_covs2.sh
@@ -10,6 +10,7 @@
 ##SBATCH --array=0-17 # nonkp_bright_faint_priority
 ##SBATCH --array=0-13 # nonkp_bright_compare or legacy
 #SBATCH --array=7-9 # nonkp_bright_compare re-run
+##SBATCH --array=0-5 # nonkp_meeting_20260521_bright_faint or nonkp_meeting_20260521_bright20p7
 ##SBATCH --array=0-29 # pip_priority
 ##SBATCH --array=0-19 # pip_20p7
 
