b3f91ac1033cdbac4a8b19cb3ff42a9e78a82f7c
Michael Rashkovetskyi <misharash@gmail.com>
2026-07-13 10:59:18 -0700

    Use global desi-clustering

--- DESI/Y3/post/updated/run_covs_test.sh
+++ DESI/Y3/post/updated/run_covs_test.sh
@@ -2,7 +2,7 @@
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
-module unload desi-clustering
+# module unload desi-clustering # use locally installed desi-clustering if uncommented, otherwise use the global one from cosmodesi environment
 
 for i in {0..13}; do
     echo ID $i
