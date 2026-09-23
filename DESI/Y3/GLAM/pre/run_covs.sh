6143b6dd783208a6c6aa948d46ec03c056829502
Michael Rashkovetskyi <misharash@gmail.com>
2026-09-23 14:00:24 -0700

    Prepared combined tracer for GLAM pre-recon

--- DESI/Y3/GLAM/pre/run_covs.sh
+++ DESI/Y3/GLAM/pre/run_covs.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=128 # 128 hyperthreads = 64 physical cores
 #SBATCH --job-name=RascalC-Y3-GLAM
 ##SBATCH --array=0-11,14,15,22,23 # LRG, ELG, LRG+ELG, BGS_BRIGHT-21.35 z0.1-0.4, and QSO
-#SBATCH --array=14,15 # BGS_BRIGHT-21.35 z0.1-0.4
+#SBATCH --array=10-11 # LRG+ELG
 
 # load cosmodesi environment
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
--- DESI/Y3/GLAM/pre/run_covs_test.sh
+++ DESI/Y3/GLAM/pre/run_covs_test.sh
@@ -4,8 +4,8 @@
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 module unload desi-clustering # temporarily use custom desi-clustering
 
-# for i in {{0..9},14,15,22,23}; do
-for i in {14,15}; do
+# for i in {{0..11},14,15,22,23}; do
+for i in {10,11}; do
     echo ID $i
     python -u run_covs.py -t $i
 done
